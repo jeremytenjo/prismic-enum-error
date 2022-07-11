083dd2521e52a0a821de47273906d926e1ef8192
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-11 15:04:43 -0400

    prod error

---
+++ src/pages/slice-simulator.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SliceSimulator } from '@prismicio/slice-simulator-react'
+import { SliceZone } from '@prismicio/react'
+
+import state from '../../.slicemachine/libraries-state.json' assert { type: 'json' }
+import { components } from '../../slices'
+
+const SliceSimulatorPage = () => (
+  <SliceSimulator
+    // The "sliceZone" prop should be a function receiving slices and rendering them using your "SliceZone" component.
+    sliceZone={(props) => <SliceZone {...props} components={components} />}
+    state={state}
+  />
+)
+
+export default SliceSimulatorPage
